e0e8cab4ea9352fdc1d2fe525b3b0abaa22758e6
didier300110500 <43076393+didier300110500@users.noreply.github.com>
2019-12-05 14:20:09 -0500

    Update 300110500-data.sql

--- Q.Query/300110500/300110500-data.sql
+++ Q.Query/300110500/300110500-data.sql
@@ -13,7 +13,9 @@ INSERT INTO HONORAIRE_PAYMENTS (Honoraire_Payment, Periode, Montant, Commentaire
 INSERT INTO HONORAIRE_PAYMENTS (Honoraire_Payment, Periode, Montant, Commentaire) VALUES ('105', '2019-12-9', '1500', 'Cheque');
 
 INSERT INTO VILLE_RESIDENCE (Code, Ville_Residence, Ville_immigration) VALUES ('AAAA', 'Ouagadougou', 'New_York'); 
-INSERT INTO VILLE_RESIDENCE () VALUES ('AAAB', 'Portugal', 'Europe'); 
+INSERT INTO VILLE_RESIDENCE (Code, Ville_Residence, Ville_immigration) VALUES ('AAAB', 'Abuja', 'Toronto'); 
+INSERT INTO VILLE_RESIDENCE (Code, Ville_Residence, Ville_immigration) VALUES ('AAAC', 'Alger', 'paris'); 
+
 
 INSERT INTO TYPE_SERVICES (Type_visa, Honoraire_payment, Type_service) VALUES ('1', '100', 'Documentation');
 INSERT INTO TYPE_SERVICES (Type_visa, Honoraire_payment, Type_service) VALUES ('2', '101', 'Demande_officielle');
