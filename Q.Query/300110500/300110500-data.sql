f2cd47cb71a8832e8be8a764a64330aa6a38a709
didier300110500 <43076393+didier300110500@users.noreply.github.com>
2019-12-05 12:13:21 -0500

    Update 300110500-data.sql

--- Q.Query/300110500/300110500-data.sql
+++ Q.Query/300110500/300110500-data.sql
@@ -12,8 +12,8 @@ INSERT INTO HONORAIRE_PAYMENTS ( Honoraire_Payment, Periode, Montant, Commentair
 INSERT INTO HONORAIRE_PAYMENTS (Honoraire_Payment, Periode, Montant, Commentaire) VALUES ('101', '2019-11-20', '500', 'Carte_Bancaire');
 INSERT INTO HONORAIRE_PAYMENTS (Honoraire_Payment, Periode, Montant, Commentaire) VALUES ('105', '2019-12-9', '1500', 'Cheque');
 
-INSERT INTO VILLE_RESIDENCE (Name, Pays, Continent) VALUES ('Hamidou_zongo', 'Congo', 'Afrique'); 
-INSERT INTO VILLE_RESIDENCE (Name,Pays, Continent ) VALUES ('Cristiano_Ronaldo', 'Portugal', 'Europe'); 
+INSERT INTO VILLE_RESIDENCE (Code, Ville_Residence, Ville_immigration) VALUES ('AAAA', 'Ouagadougou', 'New_York'); 
+INSERT INTO VILLE_RESIDENCE () VALUES ('AAAB', 'Portugal', 'Europe'); 
 
 INSERT INTO TYPE_SERVICES (Type_service, Duree) VALUES ('Etudiant_international', 'Temporaire');
 INSERT INTO TYPE_SERVICES (Type_service, Duree) VALUES ('Travailleur_qualifie', 'permanent');0
