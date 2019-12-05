9b638e9861db94c2285454289be92e2605dcfd14
Romeomian <43684024+Romeomian@users.noreply.github.com>
2019-12-05 15:13:30 -0500

    Update 300104541-data.sql

--- Q.Query/300104541/300104541-data.sql
+++ Q.Query/300104541/300104541-data.sql
@@ -12,15 +12,15 @@ INSERT INTO VENTE_PHYSIQUES (Ventes, vente_physique) VALUES ('En Store', 1);
 INSERT INTO VENTE_PHYSIQUES (Ventes, vente_physique) VALUES ('THE SOURCE', 2);
 INSERT INTO VENTE_PHYSIQUES (Ventes, vente_physique) VALUES ('EB GAMES', 3);
 
-INSERT INTO DIFFUSIONS (Diffusion, diffusion) VALUES ('Streaming' , 1);
-INSERT INTO DIFFUSIONS (Diffusion, diffusion) VALUES ('Mode Story' , 2);
+INSERT INTO DIFFUSIONS (Diffusions, diffusion) VALUES ('Streaming' , 1);
+INSERT INTO DIFFUSIONS (Diffusions, diffusion) VALUES ('Mode Story' , 2);
 
-INSERT INTO JEUX (Jeux, Categorie, jeux, console, diffusion) VALUES ('Modern Combat', 'Action', 1, 1, 2);
-INSERT INTO JEUX (Jeux, Categorie, jeux, console, diffusion) VALUES ('FIFA19', 'Sport', 2, 3, 1);
-INSERT INTO JEUX (Jeux, Categorie, jeux, console, diffusion) VALUES ('Naruto', 'Adventure', 3, 2, 2);
-INSERT INTO JEUX (Jeux, Categorie, jeux, console, diffusion) VALUES (''
+INSERT INTO JEUX (Jeux_Videos, Categorie, jeux, console, diffusion) VALUES ('Modern Combat', 'Action', 1, 1, 2);
+INSERT INTO JEUX (Jeux_Videos, Categorie, jeux, console, diffusion) VALUES ('FIFA19', 'Sport', 2, 3, 1);
+INSERT INTO JEUX (Jeux_Videos, Categorie, jeux, console, diffusion) VALUES ('Naruto', 'Adventure', 3, 2, 2);
+INSERT INTO JEUX (Jeux_Videos, Categorie, jeux, console, diffusion) VALUES (''
 
-INSERT INTO SUPPORTS (support, vente_virtuelle, Support) VALUES(1, 1, 'Clavardage');
-INSERT INTO SUPPORTS (support, vente_virtuelle, Support) VALUES(2, 2, 'Appel');
-INSERT INTO SUPPORTS (support, vente_physique, Support) VALUES(3, 3, 'Guichet');  
-INSERT INTO SUPPORTS (support, vente_physique, Support) VALUES(4, 2, 'Check-Out');                                                                     
+INSERT INTO SUPPORTS (support, vente_virtuelle, Supports) VALUES(1, 1, 'Clavardage');
+INSERT INTO SUPPORTS (support, vente_virtuelle, Supports) VALUES(2, 2, 'Appel');
+INSERT INTO SUPPORTS (support, vente_physique, Supports) VALUES(3, 3, 'Guichet');  
+INSERT INTO SUPPORTS (support, vente_physique, Supports) VALUES(4, 2, 'Check-Out');                                                                     
