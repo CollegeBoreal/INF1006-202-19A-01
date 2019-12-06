cb32c86f11e604e1eb83d8416e1be2d60778b3b0
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-12-06 00:09:31 -0500

    :car:

--- Q.Query/300107361/300107361-data.sql
+++ Q.Query/300107361/300107361-data.sql
@@ -19,6 +19,7 @@ INSERT INTO ENGIN_TYPES (Engins) VALUES ('Petrol');
 INSERT INTO MAKES (make, origin, Makes) VALUES ('acura', 'japan', 'ACURA');
 INSERT INTO MAKES (make, origin, Makes) VALUES ('jeep', 'usa', 'JEEP');
 INSERT INTO MAKES (make, origin, Makes) VALUES ('lamborghini', 'italy', 'LAMBORGHINI');
+INSERT INTO MAKES (make, origin, Makes) VALUES ('ford', 'usa', 'FORD');
 
 INSERT INTO YEARS (Years) VALUES ('2019-02-20');
 INSERT INTO YEARS (Years) VALUES ('2019-01-03');
@@ -37,11 +38,16 @@ INSERT INTO PRICES (Prices) VALUES (1430000);
 INSERT INTO MODELS (make, year, colour, price, Models) VALUES ('acura', 1, 'blk', 2, 'NSX');
 INSERT INTO MODELS (make, year, colour, price, Models) VALUES ('jeep', 2, 'wht', 1, 'WRANGLER');
 INSERT INTO MODELS (make, year, colour, price, Models) VALUES ('lamborghini', 3, 'grn', 3, 'AVENTADOR');
+INSERT INTO MODELS (make, year, colour, price, Models) VALUES ('lamborghini', 2, 'blk', 2, 'PALM BEACH');
+INSERT INTO MODELS (make, year, colour, price, Models) VALUES ('ford', 1, 'gry', 1, 'MUSTANG GT');
 
-INSERT INTO CUSTOMERS (customer, Name, FirstName) VALUES ('EJ','Etienne', 'Joky');
+INSERT INTO CUSTOMERS (customer, LastName, FirstName) VALUES ('EJ', 'Etienne', 'Joky');
+INSERT INTO CUSTOMERS (customer, LastName, FirstName) VALUES ('MN', 'Noel', 'Melissa');
+INSERT INTO CUSTOMERS (customer, LastName, FirstName) VALUES ('RC', 'Ricca', 'Collins');
 
 INSERT INTO PAYMENTS (customer, price) VALUES ('EJ', 1);
-INSERT INTO PAYMENTS (customer, price) VALUES ('EJ', 2);
+INSERT INTO PAYMENTS (customer, price) VALUES ('MN', 2);
+INSERT INTO PAYMENTS (customer, price) VALUES ('RC', 3);
 
 INSERT INTO MAINTENANCES (Maintenances) VALUES ('Brake');
 INSERT INTO MAINTENANCES (Maintenances) VALUES ('Repair');
