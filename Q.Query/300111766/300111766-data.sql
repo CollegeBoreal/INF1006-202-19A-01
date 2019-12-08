0f736f9ba1fe5c1759c67e5293e33daf92d78675
cheikthiam <44683625+Cheikhthiam@users.noreply.github.com>
2019-12-07 19:55:19 -0800

    cheikh

--- Q.Query/300111766/300111766-data.sql
+++ Q.Query/300111766/300111766-data.sql
@@ -12,14 +12,23 @@ INSERT INTO MARKS ( name ) VALUES ('huawei');
 
 -- Données de la table PRODUITS
 
-INSERT INTO MODELS ( modelName ) VALUES ();
-INSERT INTO MODELS ( modelName) VALUES ();
-INSERT INTO MODELS ( modelName) VALUES ();
+INSERT INTO MODELS ( modelName ) VALUES ('iphone10');
+INSERT INTO MODELS ( modelName ) VALUES ('iphone11');
+INSERT INTO MODELS ( modelName ) VALUES ('samsung S10');
+INSERT INTO MODELS ( modelName ) VALUES ('nokia 7.2');
+INSERT INTO MODELS ( modelName ) VALUES ('huawei p30');
+
+-- Données de la table CLIENTS
+INSERT INTO INVOICES ( Nom, Date) VALUES ('olitan','2020-09-13');
+INSERT INTO INVOICES ( Nom, Date ) VALUES ('aek', '2020-09-13');
+INSERT INTO INVOICES ( Nom, Date) VALUES ('jemma', '2020-09-13');
+INSERT INTO INVOICES ( Nom, Date) VALUES ('check', '2020-09-13');
+INSERT INTO INVOICES ( Nom, Date ) VALUES ('abbas', '2020-09-13');
 
 -- Données de la table CLIENTS
 
-INSERT INTO CLIENTS ( ) VALUES (40, 'olitan', 'toronto');
-INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (50, 'aek', 'northyork');
-INSERT INTO CLIENTS (IDclient, Nom_client, ville) VALUES (60, 'jemma', 'mississauga');
-INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (70, 'check', 'toronto');
-INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (80, 'abbas', 'toronto');
+INSERT INTO CUSTOMERS ( ) VALUES (40, 'olitan', 'toronto');
+INSERT INTO CUSTOMERS ( IDclient, Nom_client, ville) VALUES (50, 'aek', 'northyork');
+INSERT INTO CUSTOMERS (IDclient, Nom_client, ville) VALUES (60, 'jemma', 'mississauga');
+INSERT INTO CUSTOMERS ( IDclient, Nom_client, ville) VALUES (70, 'check', 'toronto');
+INSERT INTO CUSTOMERS ( IDclient, Nom_client, ville) VALUES (80, 'abbas', 'toronto');
