9b2121e731788b9ea3a368336e2a32f95274a35e
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-12-08 13:18:46 -0500

    Update 300107361-schema.sql

--- Q.Query/300107361/300107361-schema.sql
+++ Q.Query/300107361/300107361-schema.sql
@@ -57,110 +57,111 @@ CREATE TABLE IF NOT EXISTS Car_Center.SERVICES_OFFERED (
 -- -----------------------------------------------------
 -- Table Car_Center.MAINTENANCES
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Car_Center`.`MAINTENANCES` (
-  `maintenance` INT(11) NOT NULL AUTO_INCREMENT,
-  `service_offered` INT(11) NULL DEFAULT NULL,
-  `Maintenances` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`maintenance`),
-  INDEX `service_offered` (`service_offered` ASC) VISIBLE,
-  CONSTRAINT `MAINTENANCES_ibfk_1`
-    FOREIGN KEY (`service_offered`)
-    REFERENCES `Car_Center`.`SERVICES_OFFERED` (`service_offered`)
+CREATE TABLE IF NOT EXISTS Car_Center.MAINTENANCES (
+  maintenance INT(11) NOT NULL AUTO_INCREMENT,
+  service_offered INT(11) NULL DEFAULT NULL,
+  Maintenances VARCHAR(255) NOT NULL,
+  PRIMARY KEY (maintenance),
+  INDEX service_offered (service_offered),
+  CONSTRAINT MAINTENANCES_ibfk_1
+    FOREIGN KEY (service_offered)
+      REFERENCES Car_Center.SERVICES_OFFERED (service_offered)
 );
 
 
 -- -----------------------------------------------------
--- Table `Car_Center`.`ORIGINS`
+-- Table Car_Center.ORIGINS
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Car_Center`.`ORIGINS` (
-  `origin` VARCHAR(30) NOT NULL,
-  `Origins` TEXT NOT NULL,
-  PRIMARY KEY (`origin`),
-  UNIQUE INDEX `origin_UNIQUE` (`origin` ASC) VISIBLE);
-
-
--- -----------------------------------------------------
--- Table `Car_Center`.`MAKES`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Car_Center`.`MAKES` (
-  `make` VARCHAR(30) NOT NULL,
-  `origin` VARCHAR(30) NOT NULL,
-  `Makes` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`make`, `origin`),
-  INDEX `origin` (`origin` ASC) VISIBLE,
-  CONSTRAINT `MAKES_ibfk_1`
-    FOREIGN KEY (`origin`)
-    REFERENCES `Car_Center`.`ORIGINS` (`origin`)
-);
-
--- -----------------------------------------------------
--- Table `Car_Center`.`YEARS`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Car_Center`.`YEARS` (
-  `year` INT(11) NOT NULL AUTO_INCREMENT,
-  `Years` DATE NULL DEFAULT NULL,
-  PRIMARY KEY (`year`)
+CREATE TABLE IF NOT EXISTS Car_Center.ORIGINS (
+  origin VARCHAR(30) NOT NULL,
+  Origins TEXT NOT NULL,
+  PRIMARY KEY (origin),
+  UNIQUE INDEX origin_UNIQUE (origin)
 );
 
 
 -- -----------------------------------------------------
--- Table `Car_Center`.`PRICES`
+-- Table Car_Center.MAKES
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Car_Center`.`PRICES` (
-  `price` INT(11) NOT NULL AUTO_INCREMENT,
-  `Prices` INT(11) NOT NULL,
-  PRIMARY KEY (`price`)
+CREATE TABLE IF NOT EXISTS Car_Center.MAKES (
+  make VARCHAR(30) NOT NULL,
+  origin VARCHAR(30) NOT NULL,
+  Makes VARCHAR(30) NOT NULL,
+  PRIMARY KEY (make, origin),
+  INDEX origin (origin),
+  CONSTRAINT MAKES_ibfk_1
+    FOREIGN KEY (origin)
+    REFERENCES Car_Center.ORIGINS (origin)
+);
+
+-- -----------------------------------------------------
+-- Table Car_Center.YEARS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Car_Center.YEARS (
+  year INT(11) NOT NULL AUTO_INCREMENT,
+  Years DATE NULL DEFAULT NULL,
+  PRIMARY KEY (year)
 );
 
 
 -- -----------------------------------------------------
--- Table `Car_Center`.`MODELS`
+-- Table Car_Center.PRICES
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Car_Center`.`MODELS` (
-  `model` INT(11) NOT NULL AUTO_INCREMENT,
-  `make` VARCHAR(30) NULL DEFAULT NULL,
-  `year` INT(11) NULL DEFAULT NULL,
-  `colour` VARCHAR(30) NULL DEFAULT NULL,
-  `price` INT(11) NOT NULL,
-  `Models` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`model`, `price`),
-  INDEX `year` (`year` ASC) VISIBLE,
-  INDEX `price` (`price` ASC) VISIBLE,
-  INDEX `make` (`make` ASC) VISIBLE,
-  INDEX `colour` (`colour` ASC) VISIBLE,
-  CONSTRAINT `MODELS_ibfk_1`
-    FOREIGN KEY (`year`)
-    REFERENCES `Car_Center`.`YEARS` (`year`),
-  CONSTRAINT `MODELS_ibfk_2`
-    FOREIGN KEY (`price`)
-    REFERENCES `Car_Center`.`PRICES` (`price`),
-  CONSTRAINT `MODELS_ibfk_3`
-    FOREIGN KEY (`make`)
-    REFERENCES `Car_Center`.`MAKES` (`make`),
-  CONSTRAINT `MODELS_ibfk_4`
-    FOREIGN KEY (`colour`)
-    REFERENCES `Car_Center`.`COLOURS` (`colour`)
+CREATE TABLE IF NOT EXISTS Car_Center.PRICES (
+  price INT(11) NOT NULL AUTO_INCREMENT,
+  Prices INT(11) NOT NULL,
+  PRIMARY KEY (price)
 );
 
 
 -- -----------------------------------------------------
--- Table `Car_Center`.`PAYMENTS`
+-- Table Car_Center.MODELS
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Car_Center`.`PAYMENTS` (
-  `payment` INT(11) NOT NULL AUTO_INCREMENT,
-  `customer` VARCHAR(4) NULL DEFAULT NULL,
-  `price` INT(11) NULL DEFAULT NULL,
-  `Amount` DECIMAL(10,0) NOT NULL DEFAULT '50000',
-  `DateOfTransaction` DATE NULL DEFAULT '2019-11-27',
-  PRIMARY KEY (`payment`),
-  INDEX `customer` (`customer` ASC) VISIBLE,
-  INDEX `price` (`price` ASC) VISIBLE,
-  CONSTRAINT `PAYMENTS_ibfk_1`
-    FOREIGN KEY (`customer`)
-    REFERENCES `Car_Center`.`CUSTOMERS` (`customer`),
-  CONSTRAINT `PAYMENTS_ibfk_2`
-    FOREIGN KEY (`price`)
-    REFERENCES `Car_Center`.`PRICES` (`price`)
+CREATE TABLE IF NOT EXISTS Car_Center.MODELS (
+  model INT(11) NOT NULL AUTO_INCREMENT,
+  make VARCHAR(30) NULL DEFAULT NULL,
+  year INT(11) NULL DEFAULT NULL,
+  colour VARCHAR(30) NULL DEFAULT NULL,
+  price INT(11) NOT NULL,
+  Models VARCHAR(50) NOT NULL,
+  PRIMARY KEY (model, price),
+  INDEX year (year),
+  INDEX price (price),
+  INDEX make (make),
+  INDEX colour (colour),
+  CONSTRAINT MODELS_ibfk_1
+    FOREIGN KEY (year)
+    REFERENCES Car_Center.YEARS (year),
+  CONSTRAINT MODELS_ibfk_2
+    FOREIGN KEY (price)
+    REFERENCES Car_Center.PRICES (price),
+  CONSTRAINT MODELS_ibfk_3
+    FOREIGN KEY (make)
+    REFERENCES Car_Center.MAKES (make),
+  CONSTRAINT MODELS_ibfk_4
+    FOREIGN KEY (colour)
+    REFERENCES Car_Center.COLOURS (colour)
+);
+
+
+-- -----------------------------------------------------
+-- Table Car_Center.PAYMENTS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Car_Center.PAYMENTS (
+  payment INT(11) NOT NULL AUTO_INCREMENT,
+  customer VARCHAR(4) NULL DEFAULT NULL,
+  price INT(11) NULL DEFAULT NULL,
+  Amount DOUBLE NOT NULL DEFAULT 50000,
+  DateOfTransaction DATE NULL DEFAULT '2019-11-27',
+  PRIMARY KEY (payment),
+  INDEX customer (customer),
+  INDEX price (price),
+  CONSTRAINT PAYMENTS_ibfk_1
+    FOREIGN KEY (customer)
+    REFERENCES Car_Center.CUSTOMERS (customer),
+  CONSTRAINT PAYMENTS_ibfk_2
+    FOREIGN KEY (price)
+    REFERENCES Car_Center.PRICES (price)
 );
 
 -- Author: <Etienne>
