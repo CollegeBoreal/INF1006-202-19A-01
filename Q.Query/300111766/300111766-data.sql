8b8f6ba87bc08e8c918c1484b6a30f4ffd5b894d
cheikthiam <44683625+Cheikhthiam@users.noreply.github.com>
2019-12-07 22:37:30 -0800

    cheikh

--- Q.Query/300111766/300111766-data.sql
+++ Q.Query/300111766/300111766-data.sql
@@ -18,19 +18,22 @@ INSERT INTO MODELS ( modelName ) VALUES ('samsung S10');
 INSERT INTO MODELS ( modelName ) VALUES ('nokia 7.2');
 INSERT INTO MODELS ( modelName ) VALUES ('huawei p30');
 
--- Données de la table CLIENTS
-INSERT INTO INVOICES ( Nom, Date) VALUES ('olitan','2020-09-13');
-INSERT INTO INVOICES ( Nom, Date ) VALUES ('aek', '2020-09-13');
-INSERT INTO INVOICES ( Nom, Date) VALUES ('jemma', '2020-09-13');
-INSERT INTO INVOICES ( Nom, Date) VALUES ('check', '2020-09-13');
-INSERT INTO INVOICES ( Nom, Date ) VALUES ('abbas', '2020-09-13');
+-- Données de la table INVOICES
+INSERT INTO INVOICES ( Nom, Date) VALUES ('iphone10', 2020-09-13);
+INSERT INTO INVOICES ( Nom, Date ) VALUES ('samsung', 2020-09-13);
+INSERT INTO INVOICES ( Nom, Date) VALUES ('huawei', 2020-09-13);
+INSERT INTO INVOICES ( Nom, Date) VALUES ('nokia', 2020-09-13);
+INSERT INTO INVOICES ( Nom, Date ) VALUES ('samsung S10', 2020-09-13);
 
--- Données de la table CLIENTS
+-- Données de la table CUSTOMERS
 INSERT INTO CUSTOMERS ( name, age ) VALUES ( 'cheikh', 26);
 INSERT INTO CUSTOMERS ( name, age ) VALUES ( 'olaitan', 28);
 INSERT INTO CUSTOMERS ( name, age ) VALUES ( 'abdel', 33);
 INSERT INTO CUSTOMERS ( name, age ) VALUES ( 'halima', 23);
-;
+
+-- Données de la table PRICES
+
+
 
 
 
