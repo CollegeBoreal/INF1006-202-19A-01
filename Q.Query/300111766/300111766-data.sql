735e1228b809dc6accd5faa73fb50ff2920f73e0
cheikthiam <44683625+Cheikhthiam@users.noreply.github.com>
2019-12-08 20:08:03 -0800

    cheikh

--- Q.Query/300111766/300111766-data.sql
+++ Q.Query/300111766/300111766-data.sql
@@ -12,11 +12,11 @@ INSERT INTO MARKS ( name ) VALUES ('huawei');
 
 -- Données de la table PRODUITS
 
-INSERT INTO MODELS ( modelName ) VALUES ('iphone10');
-INSERT INTO MODELS ( modelName ) VALUES ('iphone11');
-INSERT INTO MODELS ( modelName ) VALUES ('samsung S10');
-INSERT INTO MODELS ( modelName ) VALUES ('nokia 7.2');
-INSERT INTO MODELS ( modelName ) VALUES ('huawei p30');
+INSERT INTO MODELS (name ) VALUES ('iphone10');
+INSERT INTO MODELS (name ) VALUES ('iphone11');
+INSERT INTO MODELS (name ) VALUES ('samsung S10');
+INSERT INTO MODELS (name ) VALUES ('nokia 7.2');
+INSERT INTO MODELS (name ) VALUES ('huawei p30');
 
 -- Données de la table INVOICES
 INSERT INTO INVOICES ( Nom, Date) VALUES ('iphone10', 2020-09-13);
@@ -32,7 +32,9 @@ INSERT INTO CUSTOMERS ( name, age ) VALUES ( 'abdel', 33);
 INSERT INTO CUSTOMERS ( name, age ) VALUES ( 'halima', 23);
 
 -- Données de la table PRICES
-
+INSERT INTO PRICES (mark, model, invoice, price ) VALUES (1, 4, 3, 1000);
+INSERT INTO PRICES (mark, model, invoice, price ) VALUES (3, 5, 5, 700);
+INSERT INTO PRICES (mark, model, invoice, price ) VALUES (2, 3, 4, 500);
 
 
 
