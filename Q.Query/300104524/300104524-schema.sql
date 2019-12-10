1892f12172b25afc7eb789d4e31f4f4a9db30721
Echnaideurgeneus <43045076+Echnaideurgeneus@users.noreply.github.com>
2019-12-09 16:09:14 -0800

    Update 300104524-schema.sql

--- Q.Query/300104524/300104524-schema.sql
+++ Q.Query/300104524/300104524-schema.sql
@@ -7,27 +7,27 @@ use technical_support;
 
 CREATE TABLE CLIENTS(
 	client INT NOT NULL AUTO_INCREMENT,
-		     nom VARCHAR(30),
-		    prenom VARCHAR(20),
+        nom VARCHAR(30),
+        prenom VARCHAR(20),
 	PRIMARY KEY(client)
 );
 
 CREATE TABLE SERVICES(
 service INT AUTO_INCREMENT,
-	client INT,
 BRAND VARCHAR(30) NOT NULL,
- problem VARCHAR(30) NOT NULL,
+problem VARCHAR(30) NOT NULL,
 description VARCHAR(120),
-	PRIMARY KEY(service),
-FOREIGN KEY(client)
-REFERENCES CLIENTS(client)
+PRIMARY KEY(service)
 );
 
 CREATE TABLE REPAIRS(
 repair INT NOT NULL AUTO_INCREMENT,
+service INT,
 mac_adress VARCHAR(30),
- brand VARCHAR(30) NOT NULL,
-PRIMARY KEY(repair)
+brand VARCHAR(30) NOT NULL,
+PRIMARY KEY(repair),
+ FOREIGN KEY(service)
+   REFERENCES SERVICES(service)
 );
 
 CREATE TABLE NOTES(
@@ -37,24 +37,35 @@ documentation VARCHAR(120) NOT NULL,
 PRIMARY KEY(archivre_num)
 );
 
+CREATE TABLE VENTES(
+  vente INT NOT NULL, 
+  date_vente DATE,
+  article VARCHAR(120),
+  service INT,
+  client INT,
+  PRIMARY KEY(client, vente),
+   FOREIGN KEY (client)
+     REFERENCES CLIENTS (client),
+    FOREIGN KEY (service)
+     REFERENCES SERVICES(service)
+);
+
+
 CREATE TABLE PAYMENTS(
 commande_num INT NOT NULL AUTO_INCREMENT,
 article VARCHAR(30) NOT NULL,
 quantity INT(10),
- note INT,
+note INT,
+client INT,
+vente INT,
 PRIMARY KEY(commande_num)
-);
- 
- 
-    CREATE TABLE VENTES(
-      num_vente INT NOT NULL, 
-      date_vente DATE,
-      article INT,
-      service INT,
-      client INT,
-PRIMARY KEY(client, service),
-       FOREIGN KEY (client)
+ FOREIGN KEY (client)
      REFERENCES CLIENTS (client),
-     FOREIGN KEY (service)
-    REFERENCES SERVICES(service)
+ FOREIGN KEY (vente)
+     REFERENCES VENTES (vente),
+ FOREIGN KEY (note)
+     REFERENCES NOTES (note)
 );
+ 
+ 
+ 
