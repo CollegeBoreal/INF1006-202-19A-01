0a746b48e7c67e4febb2e7987d7c1b5fdc40f50b
BGBG <43278130+BgbgL13@users.noreply.github.com>
2019-12-09 17:14:55 -0800

    Update 300105201-data.sql

--- Q.Query/300105201/300105201-data.sql
+++ Q.Query/300105201/300105201-data.sql
@@ -7,6 +7,10 @@ use musicshop;
 INSERT INTO SERVICES_OFFERED (service_offered, Services) VALUES (1, 'Instruments');
 
 INSERT INTO CUSTOMERS (customer, Name, Phone_number) VALUES (1, 'John', 64773);
+INSERT INTO CUSTOMERS (customer, Name, Phone_number) VALUES (2, 'Jacques', 64774);
+INSERT INTO CUSTOMERS (customer, Name, Phone_number) VALUES (3, 'Carl', 64778);
+INSERT INTO CUSTOMERS (customer, Name, Phone_number) VALUES (4, 'Johnny', 64777);
+INSERT INTO CUSTOMERS (customer, Name, Phone_number) VALUES (5, 'Joseph', 64772);
 
 INSERT INTO INSTRUMENTS (instrument, Types, Name) VALUES (1, 'Percusion', 'Piano');
 INSERT INTO INSTRUMENTS (instrument, Types, Name) VALUES (2, 'Strings', 'Guitar');
