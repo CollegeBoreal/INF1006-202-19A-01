e8a2dd8e8f949d0f77b5cda53337b8ed55000f81
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-12-11 14:32:46 -0500

    🎡

--- Q.Query/300108234/300108234-schema.sql
+++ Q.Query/300108234/300108234-schema.sql
@@ -1,122 +1,140 @@
--- Mon domaine
-	
+-- -----------------------------------------------------
+-- Schema Hijabfashion
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Hijabfashion;
 
-CREATE DATABASE IF NOT EXISTS Hijabfashion;
-	
+-- -----------------------------------------------------
 -- Mon utilisateur
-	
-
+-- -----------------------------------------------------
 CREATE USER IF NOT EXISTS 'halima'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL ON Hijabfashion.* TO 'halima'@'localhost';
 		
-
--- Selectionner la base de donnees 
-
-
-use Hijabfashion;
+USE `Hijabfashion` ;
 
 
 -- Mes tables
-	
-	 
-CREATE TABLE ADRESSES(
-adresse INT AUTO_INCREMENT,
-rue VARCHAR(30) NOT NULL,
-ville VARCHAR(30) NOT NULL,
-pays VARCHAR(30) NOT NULL,
-code_postal VARCHAR(30) NOT NULL,
-PRIMARY KEY (adresse)
 
+-- -----------------------------------------------------
+-- Table Hijabfashion.ACCESSOIRES
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.ACCESSOIRES(
+  accessoire INT(11) NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(30) NOT NULL,
+  type VARCHAR(30) NOT NULL,
+  PRIMARY KEY (accessoire)
 );
 
 
-CREATE TABLE VENTES(
-vente INT AUTO_INCREMENT,
-commande date,
-PRIMARY KEY (vente)
-
-);
-
-CREATE TABLE PRIX(
-prix INT AUTO_INCREMENT,
-nom VARCHAR(30) NOT NULL,
-tarif VARCHAR(30) NOT NULL,
-PRIMARY KEY (prix)
-
-
+-- -----------------------------------------------------
+-- Table Hijabfashion.ADRESSES
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.ADRESSES (
+  adresse INT(11) NOT NULL AUTO_INCREMENT,
+  rue VARCHAR(30) NOT NULL,
+  ville VARCHAR(30) NOT NULL,
+  pays VARCHAR(30) NOT NULL,
+  code_postal VARCHAR(30) NOT NULL,
+  PRIMARY KEY (adresse)
 );
 
 
-CREATE TABLE CLIENTS(
-adresse INT,
-vente INT,
-prix INT, 
-client INT,
-PRIMARY KEY (adresse, vente, prix),
-FOREIGN KEY(adresse) 
-   REFERENCES ADRESSES(adresse),
-FOREIGN KEY(vente) 
-   REFERENCES VENTES(vente),
-FOREIGN KEY(prix) 
-   REFERENCES PRIX(prix)
-
+-- -----------------------------------------------------
+-- Table Hijabfashion.VENTES
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.VENTES (
+  vente INT(11) NOT NULL AUTO_INCREMENT,
+  commande DATE NULL DEFAULT NULL,
+  PRIMARY KEY (vente)
 );
 
 
-
-CREATE TABLE COULEURS(
-couleur INT AUTO_INCREMENT, 
-nom VARCHAR(30) NOT NULL,  
-PRIMARY KEY (couleur)
-
+-- -----------------------------------------------------
+-- Table Hijabfashion.PRIX
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.PRIX (
+  prix INT(11) NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(30) NOT NULL,
+  tarif VARCHAR(30) NOT NULL,
+  PRIMARY KEY (prix)
 );
 
 
-
-CREATE TABLE TAILLES(
-taille INT AUTO_INCREMENT,
-largeur VARCHAR(30) NOT NULL,	
-longeur VARCHAR(30) NOT NULL,
-unite_de_mesure VARCHAR(30) NOT NULL,
-PRIMARY KEY (taille)
-	
+-- -----------------------------------------------------
+-- Table Hijabfashion.CLIENTS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.CLIENTS (
+  adresse INT(11) NOT NULL,
+  vente INT(11) NOT NULL,
+  prix INT(11) NOT NULL,
+  client INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (adresse, vente, prix),
+  INDEX vente (vente ASC),
+  INDEX prix (prix ASC),
+  CONSTRAINT CLIENTS_ibfk_1
+    FOREIGN KEY (adresse)
+    REFERENCES Hijabfashion.ADRESSES (adresse),
+  CONSTRAINT CLIENTS_ibfk_2
+    FOREIGN KEY (vente)
+    REFERENCES Hijabfashion.VENTES (vente),
+  CONSTRAINT CLIENTS_ibfk_3
+    FOREIGN KEY (prix)
+    REFERENCES Hijabfashion.PRIX (prix)
 );
 
+-- -----------------------------------------------------
+-- Table Hijabfashion.COULEURS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.COULEURS (
+  couleur INT(11) NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(30) NOT NULL,
+  PRIMARY KEY (couleur))
 
-CREATE TABLE ACCESSOIRES(
-accessoire INT AUTO_INCREMENT,  
-nom VARCHAR(30) NOT NULL,
-type VARCHAR(30) NOT NULL,
-PRIMARY KEY (accessoire)
 
+-- -----------------------------------------------------
+-- Table Hijabfashion.PAYS_DE_FABRICATION
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.PAYS_DE_FABRICATION (
+  pays_de_fabrication INT(11) NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(30) NOT NULL,
+  devise VARCHAR(30) NOT NULL,
+  livraison DATE NULL DEFAULT NULL,
+  PRIMARY KEY (pays_de_fabrication)
+);
+-- -----------------------------------------------------
+-- Table Hijabfashion.TAILLES
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.TAILLES (
+  taille INT(11) NOT NULL AUTO_INCREMENT,
+  largeur VARCHAR(30) NOT NULL,
+  longeur VARCHAR(30) NOT NULL,
+  unite_de_mesure VARCHAR(30) NOT NULL,
+  PRIMARY KEY (taille)
 );
 
-
-CREATE TABLE PAYS_DE_FABRICATION( 
-pays_de_fabrication INT AUTO_INCREMENT,  
-nom VARCHAR(30) NOT NULL,
-devise VARCHAR(30) NOT NULL,
-livraison Date,
-PRIMARY KEY (pays_de_fabrication)
-
+-- -----------------------------------------------------
+-- Table Hijabfashion.TISSUS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Hijabfashion.TISSUS (
+  taille INT(11) NOT NULL,
+  couleur INT(11) NOT NULL,
+  accessoire INT(11) NOT NULL,
+  pays_de_fabrication INT(11) NOT NULL,
+  tissue INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (taille, couleur, accessoire, pays_de_fabrication),
+  INDEX couleur (couleur ASC),
+  INDEX accessoire (accessoire ASC),
+  INDEX pays_de_fabrication (pays_de_fabrication ASC),
+  CONSTRAINT TISSUS_ibfk_1
+    FOREIGN KEY (taille)
+    REFERENCES Hijabfashion.TAILLES (taille),
+  CONSTRAINT TISSUS_ibfk_2
+    FOREIGN KEY (couleur)
+    REFERENCES Hijabfashion.COULEURS(couleur),
+  CONSTRAINT TISSUS_ibfk_3
+    FOREIGN KEY (accessoire)
+    REFERENCES Hijabfashion.ACCESSOIRES (accessoire),
+  CONSTRAINT TISSUS_ibfk_4
+    FOREIGN KEY (pays_de_fabrication)
+    REFERENCES Hijabfashion.PAYS_DE_FABRICATION (pays_de_fabrication)
 );
-
-
-CREATE TABLE TISSUS(
-taille INT,
-couleur INT,
-accessoire INT,
-pays_de_fabrication INT,
-tissu INT,
-
-PRIMARY KEY ( taille, couleur, accessoire, pays_de_fabrication),
-FOREIGN KEY(taille) 
-   REFERENCES TAILLES(taille),
-FOREIGN KEY(couleur) 
-   REFERENCES COULEURS(couleur),
-FOREIGN KEY(accessoire) 
-   REFERENCES ACCESSOIRES(accessoire),
-FOREIGN KEY(pays_de_fabrication) 
-   REFERENCES PAYS_DE_FABRICATION(pays_de_fabrication)
-
-);
+	       
