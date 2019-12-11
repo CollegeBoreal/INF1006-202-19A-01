56a2d02588d12b32307f258e37ce10250e19d243
BGBG <43278130+BgbgL13@users.noreply.github.com>
2019-12-10 17:18:06 -0800

    Update 300105201-schema.sql

--- Q.Query/300105201/300105201-schema.sql
+++ Q.Query/300105201/300105201-schema.sql
@@ -15,43 +15,29 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema musicshop
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `musicshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `musicshop` ;
+USE musicshop ;
 
 -- -----------------------------------------------------
--- Table `musicshop`.`COLOURS`
+-- Table musicshop.COLOURS
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `musicshop`.`COLOURS` (
-  `colour` INT(11) NOT NULL,
-  `Colors` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`colour`))
+CREATE TABLE IF NOT EXISTS musicshop.COLOURS (
+  colour INT(11) NOT NULL,
+  Colors VARCHAR(30) NOT NULL,
+  PRIMARY KEY (colour))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `musicshop`.`CUSTOMERS`
+-- Table musicshop.CUSTOMERS
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `musicshop`.`CUSTOMERS` (
-  `customer` INT(11) NOT NULL AUTO_INCREMENT,
-  `customers` VARCHAR(40) NULL DEFAULT NULL,
-  `Name` VARCHAR(30) NOT NULL,
-  `Phone_number` INT(11) NOT NULL,
-  PRIMARY KEY (`customer`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 7
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `musicshop`.`INSTRUMENTS`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `musicshop`.`INSTRUMENTS` (
-  `instrument` INT(11) NOT NULL AUTO_INCREMENT,
-  `Types` TEXT NOT NULL,
-  `Name` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`instrument`))
+CREATE TABLE IF NOT EXISTS musicshop.CUSTOMERS (
+  customer INT(11) NOT NULL AUTO_INCREMENT,
+  customers VARCHAR(40) NULL DEFAULT NULL,
+  Name VARCHAR(30) NOT NULL,
+  Phone_number INT(11) NOT NULL,
+  PRIMARY KEY (customer))
 ENGINE = InnoDB
 AUTO_INCREMENT = 7
 DEFAULT CHARACTER SET = utf8mb4
@@ -59,22 +45,13 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `musicshop`.`PRICES`
+-- Table musicshop.INSTRUMENTS
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `musicshop`.`PRICES` (
-  `price` INT(11) NOT NULL AUTO_INCREMENT,
-  `instrument` INT(11) NOT NULL,
-  `colour` INT(11) NULL DEFAULT NULL,
-  `Prices` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`price`, `instrument`),
-  INDEX `instrument` (`instrument`),
-  INDEX `colour` (`colour`),
-  CONSTRAINT `PRICES_ibfk_1`
-    FOREIGN KEY (`instrument`)
-    REFERENCES `musicshop`.`INSTRUMENTS` (`instrument`),
-  CONSTRAINT `PRICES_ibfk_2`
-    FOREIGN KEY (`colour`)
-    REFERENCES `musicshop`.`COLOURS` (`colour`))
+CREATE TABLE IF NOT EXISTS musicshop.INSTRUMENTS (
+  instrument INT(11) NOT NULL AUTO_INCREMENT,
+  Types TEXT NOT NULL,
+  Name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (instrument))
 ENGINE = InnoDB
 AUTO_INCREMENT = 7
 DEFAULT CHARACTER SET = utf8mb4
@@ -82,33 +59,56 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `musicshop`.`PAYMENTS`
+-- Table musicshop.PRICES
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `musicshop`.`PAYMENTS` (
-  `payment` INT(11) NOT NULL AUTO_INCREMENT,
-  `transaction_date` DATE NULL DEFAULT NULL,
-  `price` INT(11) NULL DEFAULT NULL,
-  `customer` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`payment`),
-  INDEX `price` (`price`),
-  INDEX `customer` (`customer'),
-  CONSTRAINT `PAYMENTS_ibfk_1`
-    FOREIGN KEY (`price`)
-    REFERENCES `musicshop`.`PRICES` (`price`),
-  CONSTRAINT `PAYMENTS_ibfk_2`
-    FOREIGN KEY (`customer`)
-    REFERENCES `musicshop`.`CUSTOMERS` (`customer`))
+CREATE TABLE IF NOT EXISTS musicshop.PRICES (
+  price INT(11) NOT NULL AUTO_INCREMENT,
+  instrument INT(11) NOT NULL,
+  colour INT(11) NULL DEFAULT NULL,
+  Prices INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (price, instrument),
+  INDEX instrument (instrument),
+  INDEX colour (colour),
+  CONSTRAINT PRICES_ibfk_1
+    FOREIGN KEY (instrument)
+    REFERENCES musicshop.INSTRUMENTS (instrument),
+  CONSTRAINT PRICES_ibfk_2
+    FOREIGN KEY (colour)
+    REFERENCES musicshop.COLOURS (colour))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table musicshop.PAYMENTS
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS musicshop.PAYMENTS (
+  payment INT(11) NOT NULL AUTO_INCREMENT,
+  transaction_date DATE NULL DEFAULT NULL,
+  price INT(11) NULL DEFAULT NULL,
+  customer INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (payment),
+  INDEX price (price),
+  INDEX customer (customer),
+  CONSTRAINT PAYMENTS_ibfk_1
+    FOREIGN KEY (price)
+    REFERENCES musicshop.PRICES (price),
+  CONSTRAINT PAYMENTS_ibfk_2
+    FOREIGN KEY (customer)
+    REFERENCES musicshop.CUSTOMERS (customer))
 ENGINE = InnoDB
 AUTO_INCREMENT = 7
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
--- Table `musicshop`.`SERVICES_OFFERED`
+-- Table musicshop.SERVICES_OFFERED
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `musicshop`.`SERVICES_OFFERED` (
-  `service_offered` INT(11) NOT NULL,
-  `Services` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`service_offered`))
+CREATE TABLE IF NOT EXISTS musicshop.SERVICES_OFFERED (
+  service_offered INT(11) NOT NULL,
+  Services VARCHAR(30) NOT NULL,
+  PRIMARY KEY (service_offered))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
