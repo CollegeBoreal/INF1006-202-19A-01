b00016da31b6545779c49a04edf2de6746349599
jthn9022 <43135622+jthn9022@users.noreply.github.com>
2019-12-10 20:59:35 -0500

    Update 300112687-data.sql

--- Q.Query/300112687/300112687-data.sql
+++ Q.Query/300112687/300112687-data.sql
@@ -11,14 +11,11 @@ INSERT INTO CLIENTS ( name,num ) VALUES ('Abas',  0003);
 
 
 -- Données de la table ABONNEMEMT
-INSERT INTO ABONNEMENT ( nom,abonnement) VALUES ('Abas','abas');
-INSERT INTO ABONNEMENT ( nom,abonnement) VALUES ('ORDEN','orden');
-INSERT INTO ABONNEMENT ( nom,abonnement ) VALUES ('JOSEPH','joseph');
+INSERT INTO ABONNEMENT ( nom,abonnement,client) VALUES ('Abas','abas',1);
+INSERT INTO ABONNEMENT ( nom,abonnement,client) VALUES ('ORDEN','orden',2);
+INSERT INTO ABONNEMENT ( nom,abonnement,client) VALUES ('JOSEPH','joseph',3);
 
--- Données de la table PAIEMENT
-INSERT INTO PAIEMENT ( nom,date,client ) VALUES ('lion','2019-02-11',1);
-INSERT INTO PAIEMENT ( nom,date,client ) VALUES ('jonathan','2019-01-22',2);
-INSERT INTO PAIEMENT  ( nom,date,client ) VALUES ('freddy','2019-04-23',3);
+
 
 -- Données  de la table SERVICES
 INSERT INTO SERVICES ( incendie,voyage,voiture ) VALUES ('maison','bagage','accident');
@@ -33,6 +30,11 @@ INSERT INTO PRIX  ( client,price) VALUES (3,500);
 INSERT INTO VENTES ( Abonnement ) VALUES ('iphone');
 INSERT INTO VENTES ( Abonnement ) VALUES ('samsung');
 INSERT INTO VENTES ( Abonnement ) VALUES ('nokia');
+
+-- Données de la table PAIEMENT
+INSERT INTO PAIEMENT ( nom,date,client,vente ) VALUES ('lion','2019-02-11',1,1);
+INSERT INTO PAIEMENT ( nom,date,client,vente) VALUES ('jonathan','2019-01-22',2,2);
+INSERT INTO PAIEMENT  ( nom,date,client,vente) VALUES ('freddy','2019-04-23',3,3);
 
 -- Données de la table adresses
 INSERT INTO ADRESSES ( rue,ville,pays,code_postal,client ) VALUES ('boucherville','qubec','canada','mapoto',1);
