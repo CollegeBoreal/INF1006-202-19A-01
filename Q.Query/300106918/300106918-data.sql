c4f2dbd2416b14f0c66938852029a2dee0259430
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2019-12-11 04:52:32 -0500

    Update 300106918-data.sql

--- Q.Query/300106918/300106918-data.sql
+++ Q.Query/300106918/300106918-data.sql
@@ -17,6 +17,7 @@ INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock ) VALUES (20
 INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (30, 'camera', '2014-10-01', 144);
 INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (40, 'camera', '2014-10-01', 150);
 INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (50, 'camera', '2014-10-01', 166);
+INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (60, 'camera', '2014-10-01', 6);
 
 -- Données de la table CLIENTS
 
@@ -25,6 +26,7 @@ INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (50, 'aek', 'northyork
 INSERT INTO CLIENTS (IDclient, Nom_client, ville) VALUES (60, 'jemma', 'mississauga');
 INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (70, 'check', 'toronto');
 INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (80, 'abbas', 'toronto');
+INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (90, 'amir', 'sudbury');
 
 -- Données de la table COMMANDES
 
@@ -33,6 +35,8 @@ INSERT INTO COMMANDES (IDfacture, IDproduit, IDclient, Quantit, Date_livraison) 
 INSERT INTO COMMANDES (IDfacture, IDproduit, IDclient, Quantit, Date_livraison) VALUES (3000, 30, 60, 100, '2020-09-13');
 INSERT INTO COMMANDES (IDfacture, IDproduit, IDclient, Quantit, Date_livraison) VALUES (3500, 40, 70, 200, '2019-01-01');
 INSERT INTO COMMANDES (IDfacture, IDproduit, IDclient, Quantit, Date_livraison) VALUES (4000, 50, 80, 100, '2020-12-09');
+INSERT INTO COMMANDES (IDfacture, IDproduit, IDclient, Quantit, Date_livraison) VALUES (4500, 60, 90, 10, '2019-12-09');
+
 
 -- Données de la table insttalation
 
