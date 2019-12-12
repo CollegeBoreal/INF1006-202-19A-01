e27839c658e1ef3a403a660a90843a348113238b
jthn9022 <43135622+jthn9022@users.noreply.github.com>
2019-12-11 19:00:31 -0500

    Update 300112687-schema.sql

--- Q.Query/300112687/300112687-schema.sql
+++ Q.Query/300112687/300112687-schema.sql
@@ -13,67 +13,104 @@ CREATE DATABASE IF NOT EXISTS Assurance;
  -- Selectionne la base de donnees
  
  
- use Assurance 
+ use Assurance;
  
  -- Mes Tables
 
   
-  CREATE TABLE CLIENTS(
-   client INT AUTO_INCREMENT,
-   name VARCHAR(30) NOT NULL,
-    num INT, 
-    PRIMARY KEY (client)
-    
- );
- 
- 
- CREATE TABLE ABONNEMENT (
- nom  VARCHAR(30) NOT NULL,
- abonnement VARCHAR(30) NOT NULL,
- PRIMARY KEY (abonnement)
- );
- 
- 
- CREATE TABLE PAIEMENT(
-  paiement INT AUTO_INCREMENT,
-  nom  VARCHAR(40) NOT NULL, 
-  date DATE,
-  client INT,
-  PRIMARY KEY (paiement,client),
-   FOREIGN KEY (client)
-     REFERENCES CLIENTS(client)
- );
- 
- CREATE TABLE SERVICES(
-  service INT AUTO_INCREMENT,
+  
+
+-- -----------------------------------------------------
+-- Table `Assurance`.`ABONNEMENT`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Assurance.ABONNEMENT (
+  nom VARCHAR(30) NOT NULL,
+  abonnement VARCHAR(30) NOT NULL,
+  PRIMARY KEY (abonnement))
+;
+
+
+-- -----------------------------------------------------
+-- Table `Assurance`.`CLIENTS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Assurance. CLIENTS (
+  client INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  num INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (client))
+;
+
+
+-- -----------------------------------------------------
+-- Table `Assurance`.`ADRESSES`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Assurance.ADRESSES (
+  adresse INT(11) NOT NULL AUTO_INCREMENT,
+  rue  VARCHAR(30) NOT NULL,
+  ville VARCHAR(30) NOT NULL,
+  pays VARCHAR(30) NOT NULL,
+  client INT(11) NOT NULL,
+  code_postal VARCHAR(30) NOT NULL,
+  PRIMARY KEY (adresse, client),
+  INDEX client (client ),
+  CONSTRAINT ADRESSES_ibfk_1
+    FOREIGN KEY (client)
+    REFERENCES Assurance.CLIENTS (client))
+;
+
+
+-- -----------------------------------------------------
+-- Table `Assurance`.`PAIEMENT`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Assurance.PAIEMENT (
+  paiement INT(11) NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(40) NOT NULL,
+  date DATE NULL DEFAULT NULL,
+  client INT(11) NOT NULL,
+  PRIMARY KEY (paiement, client),
+  INDEX client (client),
+  CONSTRAINT PAIEMENT_ibfk_1
+    FOREIGN KEY (client)
+    REFERENCES Assurance.CLIENTS (client))
+;
+
+
+-- -----------------------------------------------------
+-- Table `Assurance`.`PRIX`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Assurance.PRIX (
+  prix INT(11) NOT NULL AUTO_INCREMENT,
+  price INT(11) NULL DEFAULT NULL,
+  client INT(11) NOT NULL,
+  PRIMARY KEY (prix, client),
+  INDEX client (client ) ,
+  CONSTRAINT PRIX_ibfk_1
+    FOREIGN KEY (client)
+    REFERENCES Assurance.CLIENTS (client))
+;
+
+
+-- -----------------------------------------------------
+-- Table `Assurance`.`SERVICES`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Assurance.SERVICES (
+  service INT(11) NOT NULL AUTO_INCREMENT,
   Incendie VARCHAR(20) NOT NULL,
   voyage VARCHAR(20) NOT NULL,
   voiture VARCHAR(30) NOT NULL,
-  PRIMARY KEY (service)
- );
- 
- CREATE TABLE PRIX (
- prix  INT AUTO_INCREMENT,
- price INT,
- client INT,
- PRIMARY KEY (prix,client),
-  FOREIGN KEY (client)
-     REFERENCES CLIENTS(client)
- );
-CREATE TABLE VENTES(
-vente INT AUTO_INCREMENT,
-Abonnement VARCHAR(30) NOT NULL,
-PRIMARY KEY (vente)
-);
-CREATE TABLE ADRESSES(
- adresse INT AUTO_INCREMENT,
-rue VARCHAR(30) NOT NULL,
-ville VARCHAR(30) NOT NULL,
-pays VARCHAR(30) NOT NULL,
- client INT,
-code_postal VARCHAR(30) NOT NULL,
-PRIMARY KEY (adresse,client),
- FOREIGN KEY (client)
-     REFERENCES CLIENTS(client)
- 
-);
+  PRIMARY KEY (service),
+  UNIQUE INDEX service_UNIQUE (service ) )
+;
+
+
+-- -----------------------------------------------------
+-- Table `Assurance`.`VENTES`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Assurance.VENTES (
+  vente INT(11) NOT NULL AUTO_INCREMENT,
+  Abonnement VARCHAR(30) NOT NULL,
+  PRIMARY KEY (vente))
+;
+
+
+
